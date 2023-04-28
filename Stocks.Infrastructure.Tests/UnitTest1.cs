da2ba47bb3e205f322ab0555a97757cb18c57028
Edgardo Ramirez <edgardo.ramirez94@gmail.com>
2023-04-28 15:29:36 -0400

    Create and reference test projects for Services and Infrastructure Layers

---
+++ Stocks.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Stocks.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Stocks.Services.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Stocks.Services.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
