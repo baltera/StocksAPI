e34a6c63c4f7c48061fff9e6377978edac766f2f
Edgardo Ramirez <edgardo.ramirez94@gmail.com>
2023-05-17 16:53:19 -0400

    Provide Api Settings trough configuration file

--- Stocks.Infrastructure/Commons/StocksHttpClient.cs
+++ Stocks.Infrastructure/Commons/StocksHttpClient.cs
@@ -4,14 +4,18 @@ namespace Stocks.Infrastructure.Commons
 {
     public class StocksHttpClient : HttpClient
     {
-        public StocksHttpClient()
+        private readonly string _apiKey;
+
+        public StocksHttpClient(string baseUri, string apiKey)
         {
-            this.BaseAddress = new Uri("https://api.stockdata.org/v1/data/");
+            //this.BaseAddress = new Uri("https://api.stockdata.org/v1/data/");            
+            this.BaseAddress = new Uri(baseUri);
+            this._apiKey = apiKey;
         }
 
         public async Task<T> GetAsync<T>(string uri)
         {
-            HttpResponseMessage response = await GetAsync($"{uri}&api_token="); //FIXME: Include API token
+            HttpResponseMessage response = await GetAsync($"{uri}&api_token=" + _apiKey);
             string jsonResponse = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<T>(jsonResponse);
         }
