ea02a333ff4cd0271951e8b6e03bc999da55b779
Edgardo Ramirez <edgardo.ramirez94@gmail.com>
2023-05-18 11:29:29 -0400

    Add simple health controller to validate deployment

---
+++ Stocks.API/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Stocks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok(new
+            {
+                message = "Endpoint working fine."
+            });
+        }
+    }
+}
